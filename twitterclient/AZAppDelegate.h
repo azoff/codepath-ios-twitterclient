81b8b02461536f1514aaf8fabebd10298316fa44
Jonathan Azoff <jon@azoffdesign.com>
2014-03-28 10:15:41 -0700

    Initial Commit

---
+++ twitterclient/AZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AZAppDelegate.h
+//  twitterclient
+//
+//  Created by Jonathan Azoff on 3/28/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitterclient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twitterclient
+//
+//  Created by Jonathan Azoff on 3/28/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AZAppDelegate class]));
+    }
+}
